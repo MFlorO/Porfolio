17da71af9a79569fabfb74a6fb35e020eb98b3ba
Flori O <floroldani@hotmail.com>
2022-10-03 23:30:11 -0300

    refactorizacion de codigo

--- src/componentes/Skills/Skills.jsx
+++ src/componentes/Skills/Skills.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
-
-//MUI
-import { IconButton, Stack, Typography } from "@mui/material/";
+import skillsAll from "./skills.js"
 
 //Translation
 import { useTranslation } from "react-i18next";
 
-//React Icons
-import { CgFigma } from "react-icons/cg"
-import {SiCss3, SiChakraui, SiHtml5, SiRedux, SiSequelize, SiPostgresql, SiTrello, SiJavascript} from "react-icons/si"
-import { FaReact } from "react-icons/fa"
-import {DiNodejs} from "react-icons/di"
-import {HiOutlineMailOpen} from "react-icons/hi"
+//MUI
+import { IconButton, Stack, Typography } from "@mui/material/";
+import { styled } from '@mui/material/styles';
 
 //React Reveal
 import Zoom from 'react-reveal/Zoom';
 
-import { styled } from '@mui/material/styles';
 
 
 
@@ -26,11 +20,12 @@ const ButtonStyles = styled(IconButton)(({ theme }) => ({
 }));
 
 const TypographyStyles = styled(Typography)(({ theme }) => ({
-
   color:'text.secondary',
   fontFamily: "Raleway",
   fontWeight: 600,
 }));
+
+
 
 
 function Skills() {
@@ -45,19 +40,10 @@ function Skills() {
       backgroundColor="background.dark"
       sx={{
         width: "100%",
-        height: {
-          xs: "80vh",
-          sm: "100vh",
-          md: "100vh",
-          lg: "100vh",
-          xl: "100vh",
-        },
-        paddingTop: {
-          xs: "2rem",
-          xl: "0rem",
-        },
-      }}
-    >
+        height: { xs: "80vh", sm: "100vh", md: "100vh", lg: "100vh", xl: "100vh" },
+        paddingTop: { xs: "2rem", xl: "0rem" }
+      }}>
+
       <Stack
         direction="column"
         justifyContent="space-around"
@@ -65,22 +51,9 @@ function Skills() {
         marginRight="5.5rem"
         sx={{
           width: "90%",
-          height: {
-            xs: "40rem",
-            sm: "35rem",
-            md: "35rem",
-            lg: "35rem",
-            xl: "35rem",
-            xxl: "35rem",
-          },
-          marginTop: {
-            xs: "5rem",
-            sm: "0rem",
-            xl: "4rem",
-          },
-          marginLeft: {
-            xs: "5rem",
-          },
+          height: { xs: "40rem", sm: "35rem", md: "35rem", lg: "35rem", xl: "35rem", xxl: "35rem"},
+          marginTop: { xs: "5rem", sm: "0rem", xl: "4rem" },
+          marginLeft: { xs: "5rem" },
         }}
       >
 
@@ -90,20 +63,10 @@ function Skills() {
           justifyContent="space-around"
           alignItems="center"
           sx={{
-            width: {
-              xs: "30%",
-              sm: "30%",
-              md: "30%",
-              lg: "30%",
-              xl: "30%",
-            },
-            marginLeft: {
-              xs: "1rem",
-            },
+            width: { xs: "30%", sm: "30%", md: "30%", lg: "30%", xl: "30%"},
+            marginLeft: { xs: "1rem" },
             fontSize:"5rem",
-            
-          }}
-        >
+          }}>
 
           <Typography variant="h1" color="text.secondary">{t("skills.titleOne")}</Typography>
           <Typography variant="h1" color="secondary.dark">{t("skills.titletwo")}</Typography>
@@ -119,80 +82,26 @@ function Skills() {
           textAlign="justify"
           gap="2rem"
           sx={{
-            height: {
-              xs: "24rem",
-              // sm: "150vh",
-              // md: "100vh",
-              // lg: "100vh",
-              xl: "20rem",
-            },
-            width: {
-              xs: "90%",
-              sm: "90%",
-              md: "80%",
-              lg: "70%",
-              xl: "65%",
-            },
-            marginLeft: {
-              xl: "2rem",
-            }
-          }}
-        >
+            height: { xs: "24rem", xl: "20rem" },
+            width: { xs: "90%", sm: "90%", md: "80%", lg: "70%", xl: "65%" },
+            marginLeft: { xs:"-3rem", sm:"2rem", xl: "2rem"}
+          }}>
+
           <Zoom>
-          <Stack>
-            <ButtonStyles><SiJavascript /></ButtonStyles>
-            <TypographyStyles>JAVASCRIPT</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><DiNodejs /></ButtonStyles>
-            <TypographyStyles>NODE JS</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiPostgresql /></ButtonStyles>
-            <TypographyStyles>POSTGRESQL</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiSequelize /></ButtonStyles>
-            <TypographyStyles>SEQUELIZE</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><FaReact /></ButtonStyles>
-            <TypographyStyles>REACT</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiRedux /></ButtonStyles> 
-            <TypographyStyles>REDUX</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><CgFigma /></ButtonStyles>
-            <TypographyStyles>FIGMA</TypographyStyles> 
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiChakraui /></ButtonStyles>
-            <TypographyStyles>CHAKRA</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiCss3 /></ButtonStyles>
-            <TypographyStyles>CSS</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiHtml5 /></ButtonStyles>
-            <TypographyStyles>HTML</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><HiOutlineMailOpen /></ButtonStyles>
-            <TypographyStyles>EMAIL JS</TypographyStyles>
-          </Stack>
-          <Stack>
-            <ButtonStyles><SiTrello /></ButtonStyles>
-            <TypographyStyles>TRELLO</TypographyStyles>
-          </Stack>
+         {skillsAll.map( skills => {
+            const {id,icon,text} = skills
+            return ( 
+            <Stack key={id}>
+             <ButtonStyles>{icon}</ButtonStyles>
+             <TypographyStyles>{text}</TypographyStyles>
+           </Stack>
+           )
+         })}
           </Zoom>
 
         </Stack>
       </Stack>
-      
-
+  
     </Stack>  
   )
 }
---
+++ src/componentes/Skills/skills.js
@@ -0,0 +1,78 @@
+//React Icons
+import { CgFigma } from "react-icons/cg"
+import {SiCss3, SiChakraui, SiHtml5, SiRedux, SiMaterialui, SiSequelize, SiPostgresql, SiTrello, SiJavascript} from "react-icons/si"
+import { FaReact } from "react-icons/fa"
+import {DiNodejs} from "react-icons/di"
+
+
+
+
+ const skillsAll = [
+    {
+        id: 1,
+        icon: <SiJavascript />,
+        text: "JAVASCRIPT"
+    },
+    {
+        id: 2,
+        icon: <DiNodejs />,
+        text: "NODE JS"
+    },
+    {
+        id: 3,
+        icon: <SiPostgresql />,
+        text: "POSTGRES SQL"
+    },
+
+    {
+        id: 4,
+        icon: <SiSequelize />,
+        text: "SEQUELIZE"
+    },
+
+    {
+        id: 5,
+        icon: <FaReact />,
+        text: "REACT"
+    },
+
+    {
+        id: 6,
+        icon: <SiRedux />,
+        text: "REDUX"
+    },
+
+    {
+        id: 7,
+        icon: <CgFigma />,
+        text: "FIGMA"
+    },
+    {
+        id: 8,
+        icon: <SiMaterialui />,
+        text: "MATERIAL UI"
+    },
+    {
+        id: 9,
+        icon: <SiChakraui />,
+        text: "CHAKRA UI"
+    },
+    {
+        id: 10,
+        icon: <SiCss3 />,
+        text: "CSS"
+    },
+    {
+        id: 11,
+        icon: <SiHtml5 />,
+        text: "HTML"
+    },
+    {
+        id: 12,
+        icon: <SiTrello />,
+        text: "TRELLO"
+    },
+
+ ]
+
+export default skillsAll;
