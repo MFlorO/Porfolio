1b073331703ad33e841dec7b8ea116060992caa5
Flori O <floroldani@hotmail.com>
2022-10-14 22:01:14 -0300

    refactoreo Home

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -7,51 +7,20 @@ import {  Stack } from "@mui/material/";
 import fondo from "./fondo.jpg"
 import foto from "./fotoPerfil.jpg"
 import HomeContent from "./HomeContent/HomeContent.jsx";
+import { styleOne, styleTwo } from "./home.style";
 
 
 
 function Home() {
 
   return (
-    <Stack direction="column" justifyContent={"center"} alignItems={"center"} 
-    style={{backgroundImage:`url(${fondo})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}
-    sx={{
-      width: "100%",
-      height: {
-        xs: "90vh",
-        sm: "100vh",
-        md: "100vh",
-        lg: "100vh",
-        xl: "100vh",
-      },
-    }}>
+    <Stack direction="column" justifyContent={"center"} alignItems={"center"}  sx={styleOne}
+    style={{backgroundImage:`url(${fondo})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
      
-    
-    <Stack direction="row" alignItems="center" 
-    backgroundColor="background.main"
-    justifyContent="center"
-    sx={{
-      width: "100%",
-      height: {
-        xs: "40vh",
-        sm: "70vh",
-        md: "70vh",
-        lg: "80vh",
-        xl: "80vh",
-      },
-      marginTop: {
-        xs: "-2rem",
-        sm: "5rem",
-        md: "5rem",
-        lg: "5rem",
-        xl: "5rem",
-      },
-    }}>
+      <Stack direction="row" alignItems="center" backgroundColor="background.main" justifyContent="center" sx={styleTwo}>
 
-    {foto && <HomeContent foto={foto}/> } {/*  Solo me cargue la pagina cuando ya este la foto   */}
+      {foto && <HomeContent foto={foto}/> } {/*  Solo me cargue la pagina cuando ya este la foto   */}
 
-
-        
       </Stack>
 
     </Stack>
---
+++ src/componentes/Home/home.style.js
@@ -0,0 +1,28 @@
+export const styleOne = {
+    width: "100%",
+    height: {
+      xs: "90vh",
+      sm: "100vh",
+      md: "100vh",
+      lg: "100vh",
+      xl: "100vh",
+    },
+  }
+
+export const styleTwo = {
+    width: "100%",
+    height: {
+      xs: "40vh",
+      sm: "70vh",
+      md: "70vh",
+      lg: "80vh",
+      xl: "80vh",
+    },
+    marginTop: {
+      xs: "-2rem",
+      sm: "5rem",
+      md: "5rem",
+      lg: "5rem",
+      xl: "5rem",
+    },
+  }
