64a11461ec025c17162fcc757d66e7957d113751
Flori O <floroldani@hotmail.com>
2022-10-14 22:36:24 -0300

    refactoreo de LandingPage

--- src/componentes/LandingPage/LandingPage.jsx
+++ src/componentes/LandingPage/LandingPage.jsx
@@ -12,6 +12,7 @@ import Themes from "../Theme/Theme.jsx";
 import videoPorfolio from "./videoPorfolio.mp4"
 import compuNegra from "./compuNegra.png"
 import s from "./LandingPage.module.css"
+import { stylesFive, stylesFour, stylesOne, stylesSix, stylesThree, stylesTwo } from "./landingPAge.styles.js";
 
 
 
@@ -26,115 +27,26 @@ function LandingPage({ColorModeContext}) {
     words: [`${t('landing.thanks')}`, `${t('landing.message')}`],
   })
 
-
-
   const login = () => navigate("/all")
 
 
   return (
-    <Stack
-    display="flex"
-    direction="column"
-    sx={{ 
-      width:"100%",
-      height:{
-        xs:"100vh",
-        sm:"100vh",
-        md:"100vh",
-        lg:"100vh",
-        xl:"100vh",
-        xxl: "100vh"
-      },
-      backgroundColor:"background.dark"
-    }}
-    
+    <Stack display="flex" direction="column" backgroundColor="background.dark" sx={stylesOne}>
 
-    >
       <Stack direction="row" className={s.extras}>
       <Language />
       <Themes ColorModeContext={ColorModeContext}/>
       </Stack>
 
-      <Stack 
-      display="flex"
-      direction="column"
-          sx={{      
-            direction:"column",
-            width:{
-              xs:"70%",
-              sm:"70%",
-              md:"70%",
-              lg:"70%",
-              xl:"70%"
-            },
-            height:{
-              xs:"45rem",
-              sm:"30rem",
-              md:"24rem",
-              lg:"24rem",
-              xl:"24rem",
-              xxl: "30rem"
-            },
-            marginLeft:{
-              xs:"4rem",
-              sm:"2rem",
-              md:"4rem",
-              lg: "6rem",
-              xl: "6rem"
-            },
-            marginTop:{
-              xs:"6rem",
-              sm:"10rem",
-              md:"10.rem",
-              lg: "10.5rem",
-              xl: "10.5rem"
-            },
-            backgroundColor:"background.dark"
-          }} 
-           >
+      <Stack display="flex" direction="column" backgroundColor="background.dark" sx={stylesTwo} >
 
         <Typography variant='h6' component='h1' color='text.secondary'
-        sx={{ 
-         fontSize:{
-          xs:"2rem",
-          sm:"3rem",
-          md:"3rem",
-          lg: "3.5rem",
-          xl: "3.5rem",
-          xxl: "4rem"
-        }
-        }} >{t('landing.hellow')} Florencia Oldani</Typography>
+        sx={stylesThree} >{t('landing.hellow')} Florencia Oldani</Typography>
 
         <Typography variant='h1' component='h3' color='text.primary' 
-        sx={{ 
-          fontSize:{
-           xs:"3rem",
-           sm:"4rem",
-           md:"5rem",
-           lg: "5rem",
-           xl: "5rem"
-         },
-         lineHeight: {
-          xs:"4rem",
-        },
-        marginTop: {
-         xs: "0.2rem",
-         sm:"0.7rem",
-         md:"0.6rem",
-         lg: "0.5rem",
-         xl: "0.1rem"
-       },
-         }}>Developer Full Stack</Typography>
+        sx={stylesFour}>Developer Full Stack</Typography>
          
-        <Typography variant='h5' color='secondary.dark'sx={{ 
-          marginTop:{
-           xs:"2rem",
-           sm:"3rem",
-           md:"3rem",
-           lg: "4rem",
-           xl: "3rem"
-         },
-         }}>
+        <Typography variant='h5' color='secondary.dark'sx={stylesFive}>
         <span>{text}</span>
         <Cursor />
         </Typography>
@@ -145,34 +57,10 @@ function LandingPage({ColorModeContext}) {
           <source src={videoPorfolio} type="video/mp4"/>
         </video>
         <img src={compuNegra} alt="" className={s.img} />
-        
 
       </Stack>
 
-      <Stack 
-      sx={{ 
-         marginTop:{
-          xs:"-15rem",
-          sm:"4rem",
-          md:"5rem",
-          lg: "6rem",
-          xl: "-3.5rem"
-        }, 
-         marginLeft:{
-          xs:"6rem",
-          sm:"4rem",
-          md:"5rem",
-          lg: "6rem",
-          xl: "6rem"
-        }, 
-         width:{
-          xs:"45%",
-          sm:"14%",
-          md:"13%",
-          lg: "12%",
-          xl: "12%"
-        },
-        }} >
+      <Stack sx={stylesSix} >
         <Button variant="outlined" size="large" color="secondary" onClick={login}>{t("landing.ingresar")}</Button></Stack>
       </Stack>
   );
---
+++ src/componentes/LandingPage/landingPAge.styles.js
@@ -0,0 +1,114 @@
+export const stylesOne = { 
+    width:"100%",
+    height:{
+      xs:"100vh",
+      sm:"100vh",
+      md:"100vh",
+      lg:"100vh",
+      xl:"100vh",
+      xxl: "100vh"
+    }    
+}
+
+export const stylesTwo = {      
+    direction:"column",
+    width:{
+      xs:"70%",
+      sm:"70%",
+      md:"70%",
+      lg:"70%",
+      xl:"70%"
+    },
+    height:{
+      xs:"45rem",
+      sm:"30rem",
+      md:"24rem",
+      lg:"24rem",
+      xl:"24rem",
+      xxl: "30rem"
+    },
+    marginLeft:{
+      xs:"4rem",
+      sm:"2rem",
+      md:"4rem",
+      lg: "6rem",
+      xl: "6rem",
+      xxl: "9rem"
+    },
+    marginTop:{
+      xs:"6rem",
+      sm:"10rem",
+      md:"10.rem",
+      lg: "10.5rem",
+      xl: "10.5rem"
+    },
+
+  }
+
+export const stylesThree = { 
+    fontSize:{
+     xs:"2rem",
+     sm:"3rem",
+     md:"3rem",
+     lg: "3.5rem",
+     xl: "3.5rem",
+     xxl: "4rem"
+   }
+}
+
+
+export const stylesFour = { 
+    fontSize:{
+     xs:"3rem",
+     sm:"4rem",
+     md:"5rem",
+     lg: "5rem",
+     xl: "5rem"
+   },
+   lineHeight: {
+    xs:"4rem",
+  },
+  marginTop: {
+   xs: "0.2rem",
+   sm:"0.7rem",
+   md:"0.6rem",
+   lg: "0.5rem",
+   xl: "0.1rem"
+ }
+}
+
+export const stylesFive = { 
+    marginTop:{
+     xs:"2rem",
+     sm:"3rem",
+     md:"3rem",
+     lg: "4rem",
+     xl: "3rem"
+   }
+}
+
+export const stylesSix = { 
+    marginTop:{
+     xs:"-15rem",
+     sm:"4rem",
+     md:"5rem",
+     lg: "6rem",
+     xl: "-3.5rem",
+     xxl: "-6rem"
+   }, 
+    marginLeft:{
+     xs:"6rem",
+     sm:"4rem",
+     md:"5rem",
+     lg: "6rem",
+     xl: "6rem",
+     xxl: "9rem"
+   }, 
+    width:{
+     xs:"45%",
+     sm:"14%",
+     md:"13%",
+     lg: "12%",
+     xl: "12%"
+   },
+   }
