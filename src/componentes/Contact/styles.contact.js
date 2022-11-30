b282325d6715bf9e3a9306bd05d0f194fe98a530
Flori O <floroldani@hotmail.com>
2022-11-29 21:41:21 -0300

    cambio de tamaño de formulario

--- src/componentes/Contact/styles.contact.js
+++ src/componentes/Contact/styles.contact.js
@@ -8,11 +8,11 @@ export const stylesSubConteiner = {
   width: "80%",
   height: { xs: "80vh", sm: "90vh", md: "20rem", lg: "20rem", xl: "35rem", xxl: "35rem" },
   marginTop: { xs: "0rem", sm: "0rem", md: "1rem", lg: "1rem", xl: "1rem", xxl: "1rem" },
-  marginLeft: { xs: "0rem", sm: "0rem", md: "0.5rem", lg: "0.5rem", xl: "0rem", xxl: "0rem" },
+  marginLeft: { xs: "0rem", sm: "0rem", md: "0rem", lg: "0rem", xl: "0rem", xxl: "0rem" },
 } 
 
 export const stylesConteinerItems = {
-  width: { xs: "30%", sm: "30%", md: "30%", lg: "30%", xl: "30%", xxl: "50%" },
+  width: { xs: "30%", sm: "30%", md: "30%", lg: "25%", xl: "25%", xxl: "30%" },
   height:"6rem",
   marginTop: { xs: "2rem", sm: "5rem", xl: "5rem" },
 }
