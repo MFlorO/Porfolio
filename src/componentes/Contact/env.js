b61f2c9139dd769ffa81a6ec77ac0dc00080a99b
Flori O <floroldani@hotmail.com>
2023-04-15 14:56:52 -0300

    publick key

--- src/componentes/Contact/env.js
+++ src/componentes/Contact/env.js
@@ -1,3 +1,3 @@
 export const serviceId = process.env.REACT_APP_SERVICE_ID;
 export const templateId = process.env.REACT_APP_TEMPLATE_ID;
-export const userId = process.env.REACT_APP_USER_ID;
+export const userId = 'l6i2ljgBExD7eyapF';
